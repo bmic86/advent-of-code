f05e3c09a01de8f174efa334b51a54ca629d660f
Bartosz Michalski <bmic86@users.noreply.github.com>
2022-08-21 03:01:22 +0200

    2015: Day 1 solved and prepared input data for Day 2.

---
+++ AdventOfCode/Year2015/Day01.cs
@@ -0,0 +1,49 @@
+﻿using AoCHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Year2015
+{
+    public class Day01 : BaseDay
+    {
+        private int GetFloorChange(char symbol)
+            => symbol switch
+            {
+                '(' => 1,
+                ')' => -1,
+                _ => 0,
+            };
+
+        public override ValueTask<string> Solve_1()
+        {
+            string input = File.ReadAllText(InputFilePath);
+
+            int result = input.Aggregate(0, (value, next) => value + GetFloorChange(next));
+            return new(result.ToString());
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            string input = File.ReadAllText(InputFilePath);
+
+            int currentFloor = 0;
+            int currentPosition = 1;
+            foreach (char c in input)
+            {
+                currentFloor += GetFloorChange(c);
+
+                if (currentFloor == -1)
+                {
+                    return new(currentPosition.ToString());
+                }
+
+                currentPosition++;
+            }
+
+            return new("No result.");
+        }
+    }
+}
---
+++ AdventOfCode/Year2015/Day02.cs
@@ -0,0 +1,21 @@
+﻿using AoCHelper;
+
+namespace AdventOfCode.Year2015
+{
+    public class Day02 : BaseDay
+    {
+        public override ValueTask<string> Solve_1()
+        {
+            string[] inputLines = File
+                .ReadAllText(InputFilePath)
+                .Split('\n', StringSplitOptions.RemoveEmptyEntries);
+
+            throw new NotImplementedException();
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
