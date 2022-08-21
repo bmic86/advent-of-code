c779ad57a86105b20ebda07982a3f10334809fef
Bartosz Michalski <bmic86@users.noreply.github.com>
2022-08-22 00:46:51 +0200

    2015: Day 3 completed

--- AdventOfCode/Year2015/Day03.cs
+++ AdventOfCode/Year2015/Day03.cs
@@ -2,38 +2,45 @@
 
 namespace AdventOfCode.Year2015
 {
-    public record GridCoords(int X, int Y);
+    public record struct GridCoords(int X, int Y);
 
     public class Day03 : BaseDay
     {
         public override ValueTask<string> Solve_1()
         {
             string input = File.ReadAllText(InputFilePath);
-
-            HashSet<GridCoords> visited = new() { new GridCoords(0, 0) };
-
-            int currentX = 0;
-            int currentY = 0;
-            foreach (char c in input)
-            {
-                switch (c)
-                {
-                    case '<': currentX--; break;
-                    case '>': currentX++; break;
-                    case '^': currentY++; break;
-                    case 'v': currentY--; break;
-                    default: continue;
-                }
-
-                visited.Add(new GridCoords(currentX, currentY));
-            }
-
-            return new(visited.Count.ToString());
+            return new(CountTotalVisitedHouses(input).ToString());
         }
 
         public override ValueTask<string> Solve_2()
         {
-            throw new NotImplementedException();
+            string input = File.ReadAllText(InputFilePath);
+            return new(CountTotalVisitedHouses(input, 2).ToString());
+        }
+
+        private static int CountTotalVisitedHouses(string path, int numOfSantas = 1)
+        {
+            HashSet<GridCoords> visited = new() { new GridCoords(0, 0) };
+
+            int santaIndex = 0;
+            var currentPositions = new GridCoords[numOfSantas];
+
+            foreach (char c in path)
+            {
+                switch (c)
+                {
+                    case '<': currentPositions[santaIndex].X--; break;
+                    case '>': currentPositions[santaIndex].X++; break;
+                    case '^': currentPositions[santaIndex].Y++; break;
+                    case 'v': currentPositions[santaIndex].Y--; break;
+                    default: continue;
+                }
+
+                visited.Add(currentPositions[santaIndex]);
+                santaIndex = (santaIndex + 1) % numOfSantas;
+            }
+
+            return visited.Count;
         }
     }
 }
