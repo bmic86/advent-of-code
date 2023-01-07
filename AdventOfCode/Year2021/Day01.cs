4dd55701631ae0dc3dba9b262ab0372bcfd8ccb5
Bartosz Michalski <bmic86@users.noreply.github.com>
2023-01-08 00:15:11 +0100

    2021: Day 1 completed

---
+++ AdventOfCode/Year2021/Day01.cs
@@ -0,0 +1,55 @@
+﻿using AoCHelper;
+using System.Linq;
+
+namespace AdventOfCode.Year2021
+{
+    public class Day01 : BaseDay
+    {
+        private const int SliceWindowSize = 3;
+
+        private readonly int[] _depths;
+
+        public Day01() => _depths = LoadData();
+
+        public override ValueTask<string> Solve_1()
+            => new(SumIncreasingDepths(_depths).ToString());
+
+        public override ValueTask<string> Solve_2()
+        {
+            List<int> sums = SumDepthsInSliceWindow();
+            return new(SumIncreasingDepths(sums).ToString());
+        }
+
+        private int SumIncreasingDepths(IEnumerable<int> depths)
+            => depths
+                .Skip(1)
+                .Aggregate(
+                    (Previous: depths.First(), Sum: 0),
+                    (acc, current) =>
+                    {
+                        int sum = (current > acc.Previous)
+                            ? acc.Sum + 1
+                            : acc.Sum;
+
+                        return (current, sum);
+                    })
+                .Sum;
+
+        private List<int> SumDepthsInSliceWindow()
+        {
+            List<int> depthSums = new List<int>();
+            for (int i = 0; i <= _depths.Length - SliceWindowSize; i++)
+            {
+                int sum = _depths[i..(i + SliceWindowSize)].Sum();
+                depthSums.Add(sum);
+            }
+
+            return depthSums;
+        }
+
+        private int[] LoadData()
+            => File.ReadAllLines(InputFilePath)
+                .Select(int.Parse)
+                .ToArray();
+    }
+}
