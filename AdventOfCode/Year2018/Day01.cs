295c218ad76fd0ae1d0a2aac2334a5f56dd7d22b
Bartosz Michalski <bmic86@users.noreply.github.com>
2023-01-07 01:40:52 +0100

    2018: Day 1 completed

---
+++ AdventOfCode/Year2018/Day01.cs
@@ -0,0 +1,44 @@
+﻿using AoCHelper;
+
+namespace AdventOfCode.Year2018
+{
+    public class Day01 : BaseDay
+    {
+        private readonly int[] _frequencyChanges;
+
+        public Day01()
+            => _frequencyChanges = LoadData();
+
+
+        public override ValueTask<string> Solve_1()
+            => new(_frequencyChanges.Sum().ToString());
+
+        public override ValueTask<string> Solve_2()
+        {
+            HashSet<int> uniqueFrequencies = new HashSet<int>();
+            int currentFrequency = 0;
+
+            bool run = true;
+            while (run)
+            {
+                foreach (int change in _frequencyChanges)
+                {
+                    currentFrequency += change;
+                    if (uniqueFrequencies.Contains(currentFrequency))
+                    {
+                        run = false;
+                        break;
+                    }
+                    uniqueFrequencies.Add(currentFrequency);
+                }
+            }
+
+            return new(currentFrequency.ToString());
+        }
+
+        private int[] LoadData()
+            => File.ReadAllLines(InputFilePath)
+                .Select(int.Parse)
+                .ToArray();
+    }
+}
